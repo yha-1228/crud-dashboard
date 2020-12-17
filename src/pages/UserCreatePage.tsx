d7536ca4297a526d6ac9952854d1b46b0fe7fc17
長谷川 裕大 <gssbtsm999@gmail.com>
2020-12-18 01:01:51 +0900

    U

--- src/pages/UserCreatePage.tsx
+++ src/pages/UserCreatePage.tsx
@@ -1,8 +1,12 @@
+import { error } from 'console';
 import React, { useState } from 'react';
+import { Redirect } from 'react-router-dom';
 import { Container } from '../components/Container';
 import { Layout } from '../components/Layout';
+import { usersUrl } from '../constants';
 
 type Values = { username: string; email: string };
+type Touched = { username: boolean; email: boolean };
 type Errors = { username: string; email: string };
 
 function validate(name: 'username' | 'email', value: string) {
@@ -28,7 +32,10 @@ function validate(name: 'username' | 'email', value: string) {
 }
 
 export function UserCreatePage() {
+  const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
+  const [shouldRedirect, setShouldRedirect] = useState<boolean>(false);
   const [values, setValues] = useState<Values>({ username: '', email: '' });
+  const [touched, setTouched] = useState<Touched>({ username: false, email: false });
   const [errors, setErrors] = useState<Errors>({
     username: '必須項目です。',
     email: '必須項目です。',
@@ -42,51 +49,78 @@ export function UserCreatePage() {
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
-    alert('送信しました！');
+    const isEveryValid = Object.values(errors).every((error) => error === '');
+
+    if (!isEveryValid) {
+      alert(`username: ${errors.username}\nemail:${errors.email}`);
+      return;
+    }
+
+    const data = { username: values.username, email: values.email };
+
+    setIsSubmitting(true);
+
+    fetch(usersUrl, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(data),
+    })
+      .then((res) => res.json())
+      .then(() => {
+        setIsSubmitting(false);
+        alert('送信しました！');
+        setShouldRedirect(true);
+      });
   };
 
-  return (
-    <Layout title="新規作成">
-      <Container>
-        <h1>新規作成</h1>
-        <form onSubmit={handleSubmit}>
-          <div>
+  if (shouldRedirect) {
+    return <Redirect to="/user-list" />;
+  } else {
+    return (
+      <Layout title="新規作成">
+        <Container>
+          <h1>新規作成</h1>
+          <form onSubmit={handleSubmit} noValidate>
             <div>
-              <label htmlFor="username">username</label>
+              <div>
+                <label htmlFor="username">username</label>
+              </div>
+              <div>
+                <input
+                  type="text"
+                  name="username"
+                  id="username"
+                  onChange={handleChange}
+                  value={values.username}
+                  placeholder="5文字以上"
+                />
+              </div>
             </div>
-            <div>
-              <input
-                type="text"
-                name="username"
-                id="username"
-                onChange={handleChange}
-                value={values.username}
-                placeholder="5文字以上"
-              />
-            </div>
-          </div>
 
-          <div>
             <div>
-              <label htmlFor="email">email</label>
+              <div>
+                <label htmlFor="email">email</label>
+              </div>
+              <div>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  onChange={handleChange}
+                  value={values.email}
+                  placeholder="sample@email.com"
+                />
+              </div>
             </div>
-            <div>
-              <input
-                type="email"
-                name="email"
-                id="email"
-                onChange={handleChange}
-                value={values.email}
-                placeholder="sample@email.com"
-              />
-            </div>
-          </div>
 
-          <div>
-            <button type="submit">新規登録</button>
-          </div>
-        </form>
-      </Container>
-    </Layout>
-  );
+            <div>
+              <button type="submit" disabled={isSubmitting}>
+                {isSubmitting ? '送信中...' : '新規登録'}
+              </button>
+            </div>
+          </form>
+        </Container>
+      </Layout>
+    );
+  }
 }
