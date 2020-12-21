9fc6fee37ed2069215ade1acc99b0b07882ce0c3
長谷川 裕大 <gssbtsm999@gmail.com>
2020-12-21 17:37:44 +0900

    U

--- src/components/Tables.tsx
+++ src/components/Tables.tsx
@@ -1,32 +1,32 @@
 import classnames from 'classnames';
-import './Tables.css';
+import styles from './Tables.module.css';
 
 export function TableWrapper({ children }: { children: React.ReactNode }) {
-  return <div className="TableWrapper">{children}</div>;
+  return <div className={styles.TableWrapper}>{children}</div>;
 }
 
 export function Table({ ...other }: JSX.IntrinsicElements['table']) {
-  return <table className="Table" {...other} />;
+  return <table className={styles.Table} {...other} />;
 }
 
 export function TableHead({ ...other }: JSX.IntrinsicElements['thead']) {
-  return <thead className="TableHead" {...other} />;
+  return <thead className={styles.TableHead} {...other} />;
 }
 
 export function TableBody({ ...other }: JSX.IntrinsicElements['tbody']) {
-  return <tbody className="TableBody" {...other} />;
+  return <tbody className={styles.TableBody} {...other} />;
 }
 
 export function TableHeader({
   align,
   ...other
 }: JSX.IntrinsicElements['th'] & { align?: 'left' | 'center' | 'right' }) {
-  return <th className={classnames('TableHeader', align && `text-${align}`)} {...other} />;
+  return <th className={classnames(styles['TableHeader'], align && `text-${align}`)} {...other} />;
 }
 
 export function TableData({
   align,
   ...other
 }: JSX.IntrinsicElements['th'] & { align?: 'left' | 'center' | 'right' }) {
-  return <td className={classnames('TableData', align && `text-${align}`)} {...other} />;
+  return <td className={classnames(styles['TableData'], align && `text-${align}`)} {...other} />;
 }
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,6 +4,8 @@ import { Card } from '../components/Card';
 import { Container } from '../components/Container';
 import { Layout } from '../components/Layout';
 
+import styles from './FunnyButton.module.css';
+
 export function HomePage() {
   return (
     <Layout title="Home">
@@ -11,6 +13,7 @@ export function HomePage() {
         <h1>Home</h1>
         <Card variant="primary">CSS MODULES</Card>
         <Card variant="warn">CSS MODULES</Card>
+        <button className={styles.primary}>HomeButton.module.css</button>
         <Link to="/users">ユーザー一覧</Link>
       </Container>
     </Layout>
