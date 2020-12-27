068e244dcd5ee8a2cd009740a00beed842d6f388
長谷川 裕大 <gssbtsm999@gmail.com>
2020-12-28 03:41:18 +0900

    U

--- src/components/shared/LinkButton.tsx
+++ src/components/shared/LinkButton.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import classnames from 'classnames';
-import { Link } from 'react-router-dom';
-import './LinkButton.css';
+import React from 'react'
+import classnames from 'classnames'
+import { Link } from 'react-router-dom'
+import './Button.css'
 
 export function LinkButton({
   to,
@@ -10,22 +10,18 @@ export function LinkButton({
   variant,
   ...other
 }: {
-  to: string;
-  children: React.ReactNode;
-  size?: string;
-  variant?: string;
+  to: string
+  children: React.ReactNode
+  size?: string
+  variant?: string
 }) {
   return (
     <Link
       to={to}
-      className={classnames(
-        'LinkButton',
-        size && `LinkButton--${size}`,
-        variant && `LinkButton--${variant}`
-      )}
+      className={classnames('Button', size && `Button--${size}`, variant && `Button--${variant}`)}
       {...other}
     >
       {children}
     </Link>
-  );
+  )
 }
