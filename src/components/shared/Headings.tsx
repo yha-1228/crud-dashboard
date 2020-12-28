4b624602787d9aef6c60c86dccfd31da89ecc712
長谷川 裕大 <gssbtsm999@gmail.com>
2020-12-28 20:48:17 +0900

    U

--- src/components/shared/Headings.tsx
+++ src/components/shared/Headings.tsx
@@ -1,6 +1,12 @@
 import { ComponentPropsWithoutRef } from 'react'
 import './Headings.css'
 
-export function HeadingLv1(props: ComponentPropsWithoutRef<'h1'>) {
-  return <h1 className="HeadingLv1" {...props} />
+type Props = ComponentPropsWithoutRef<'h1'> & { children: React.ReactNode }
+
+export function HeadingLv1({ children, ...other }: Props) {
+  return (
+    <h1 className="HeadingLv1" {...other}>
+      {children}
+    </h1>
+  )
 }
