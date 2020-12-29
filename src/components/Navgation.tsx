801117fd5989fd5d93ca13930ddaf772d7c0abc5
長谷川 裕大 <gssbtsm999@gmail.com>
2020-12-30 02:04:59 +0900

    U

--- src/components/Navgation.tsx
+++ src/components/Navgation.tsx
@@ -45,6 +45,22 @@ export function Navgation() {
             Site setting
           </NavLink>
         </li>
+
+        {/* test */}
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23].map(
+          (_, i) => (
+            <li className="Navgation__item">
+              <NavLink
+                className="Navgation__link"
+                activeClassName="Navgation__link--active"
+                to="/site-setting"
+                exact
+              >
+                Dummy item {i}
+              </NavLink>
+            </li>
+          )
+        )}
       </ul>
     </nav>
   )
