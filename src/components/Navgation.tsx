8f8c121f18d392c1cba39d1094c31a91e480332a
長谷川 裕大 <gssbtsm999@gmail.com>
2020-12-30 02:20:05 +0900

    U

--- src/components/Navgation.tsx
+++ src/components/Navgation.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 import './Navgation.css'
 
+const testArray = [...Array(50)].map((_, i) => i)
+
 export function Navgation() {
   return (
     <nav>
@@ -47,20 +49,18 @@ export function Navgation() {
         </li>
 
         {/* test */}
-        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23].map(
-          (_, i) => (
-            <li className="Navgation__item">
-              <NavLink
-                className="Navgation__link"
-                activeClassName="Navgation__link--active"
-                to="/site-setting"
-                exact
-              >
-                Dummy item {i}
-              </NavLink>
-            </li>
-          )
-        )}
+        {testArray.map((_, i) => (
+          <li className="Navgation__item">
+            <NavLink
+              className="Navgation__link"
+              activeClassName="Navgation__link--active"
+              to="/site-setting"
+              exact
+            >
+              Dummy item {i}
+            </NavLink>
+          </li>
+        ))}
       </ul>
     </nav>
   )
