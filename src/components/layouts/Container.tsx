ff122ff8cd406362b753ebf3a57bbe366bad798d
長谷川 裕大 <gssbtsm999@gmail.com>
2020-12-30 16:06:07 +0900

    U

--- src/components/layouts/Container.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import './Container.css'
-
-type Props = { children: React.ReactNode }
-
-export function Container({ children }: Props) {
-  return <div className="Container">{children}</div>
-}
