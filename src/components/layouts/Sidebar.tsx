707fa85912cc6d76d523725470de14061cfd7ab4
長谷川 裕大 <gssbtsm999@gmail.com>
2020-12-30 15:48:28 +0900

    U

--- src/components/layouts/Sidebar.tsx
+++ src/components/layouts/Sidebar.tsx
@@ -1,5 +1,17 @@
-import './Sidebar.css'
+import React from 'react'
+import { Box } from '@material-ui/core'
 
 export function Sidebar({ children }: { children?: React.ReactNode }) {
-  return <aside className="Sidebar">{children}</aside>
+  return (
+    <Box
+      component="aside"
+      flexGrow={0}
+      flexShrink={0}
+      width={240}
+      height="100vh"
+      bgcolor="var(--color-primary)"
+    >
+      {children}
+    </Box>
+  )
 }
