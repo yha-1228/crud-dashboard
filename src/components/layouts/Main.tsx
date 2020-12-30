d813c51b54be64c5e176b69b684a621f5fcf04bf
長谷川 裕大 <gssbtsm999@gmail.com>
2020-12-30 15:50:53 +0900

    U

--- src/components/layouts/Main.tsx
+++ src/components/layouts/Main.tsx
@@ -1,5 +1,10 @@
-import './Main.css'
+import React from 'react'
+import { Box } from '@material-ui/core'
 
 export function Main({ children }: { children: React.ReactNode }) {
-  return <div className="Main">{children}</div>
+  return (
+    <Box flexGrow={1} flexShrink={1} height="100vh">
+      {children}
+    </Box>
+  )
 }
