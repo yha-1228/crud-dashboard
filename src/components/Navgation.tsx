8414f8b12a052b2c4379d26fe3045c6ba5abbeeb
長谷川 裕大 <gssbtsm999@gmail.com>
2020-12-31 03:01:59 +0900

    U

--- src/components/Navgation.tsx
+++ src/components/Navgation.tsx
@@ -3,6 +3,14 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 import './Navgation.css'
 
+const items = [
+  { title: 'Home', to: '/', exact: true },
+  { title: 'Users', to: '/users', exact: false },
+  { title: 'Invoices', to: '/invoices', exact: true },
+  { title: 'Configs', to: '/configs', exact: true },
+  { title: 'Site setting', to: '/site-setting', exact: true },
+]
+
 function NavItem({ children }: { children: React.ReactNode }) {
   return (
     <Box component="li" mr="-24px" ml="-24px" lineHeight="40px">
@@ -15,59 +23,18 @@ export function Navgation() {
   return (
     <nav>
       <ul>
-        <NavItem>
-          <NavLink
-            className="Navgation__link"
-            activeClassName="Navgation__link--active"
-            to="/"
-            exact
-          >
-            Home
-          </NavLink>
-        </NavItem>
-
-        <NavItem>
-          <NavLink
-            className="Navgation__link"
-            activeClassName="Navgation__link--active"
-            to="/users"
-          >
-            Users
-          </NavLink>
-        </NavItem>
-
-        <NavItem>
-          <NavLink
-            className="Navgation__link"
-            activeClassName="Navgation__link--active"
-            to="/invoices"
-            exact
-          >
-            Invoices
-          </NavLink>
-        </NavItem>
-
-        <NavItem>
-          <NavLink
-            className="Navgation__link"
-            activeClassName="Navgation__link--active"
-            to="/configs"
-            exact
-          >
-            Configs
-          </NavLink>
-        </NavItem>
-
-        <NavItem>
-          <NavLink
-            className="Navgation__link"
-            activeClassName="Navgation__link--active"
-            to="/site-setting"
-            exact
-          >
-            Site setting
-          </NavLink>
-        </NavItem>
+        {items.map((item, index) => (
+          <NavItem key={index}>
+            <NavLink
+              className="Navgation__link"
+              activeClassName="Navgation__link--active"
+              to={item.to}
+              exact={item.exact}
+            >
+              {item.title}
+            </NavLink>
+          </NavItem>
+        ))}
       </ul>
     </nav>
   )
