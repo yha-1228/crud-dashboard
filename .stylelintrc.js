63fe63853d4c4083095d4365adc5b29af97764bc
長谷川 裕大 <gssbtsm999@gmail.com>
2020-12-30 22:19:03 +0900

    U

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -7,6 +7,13 @@ module.exports = {
   plugins: ['stylelint-order'],
   ignoreFiles: ['**/node_modules/**', '**/normalize.css', 'src/**/*.tsx'],
   rules: {
+    // rulesへの追加はVSC再起動しないとダメな場合がある
+    'property-no-unknown': [
+      true,
+      {
+        ignoreProperties: ['composes'],
+      },
+    ],
     'string-quotes': 'single',
   },
 }
