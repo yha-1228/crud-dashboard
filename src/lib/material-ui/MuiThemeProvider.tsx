9e45e16c7f33e54ff0dd175d382baa863d6d262e
長谷川 裕大 <gssbtsm999@gmail.com>
2020-12-31 15:24:57 +0900

    U

--- src/lib/material-ui/MuiThemeProvider.tsx
+++ src/lib/material-ui/MuiThemeProvider.tsx
@@ -4,7 +4,7 @@ import { createMuiTheme, ThemeProvider } from '@material-ui/core'
 const theme = createMuiTheme({
   palette: {
     primary: {
-      main: '#3413ec',
+      main: '#1D65F7',
     },
   },
 })
