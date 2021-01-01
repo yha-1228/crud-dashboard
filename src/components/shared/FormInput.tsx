d78fcfdc0e41e2512ec675a86fea9f3fefcb78cb
長谷川 裕大 <gssbtsm999@gmail.com>
2021-01-01 13:06:27 +0900

    U

--- src/components/shared/FormInput.tsx
+++ src/components/shared/FormInput.tsx
@@ -1,5 +1,5 @@
 import { ComponentPropsWithoutRef } from 'react'
-import styles from './Input.module.css'
+import styles from './FormInput.module.css'
 
 export function FormInput(props: ComponentPropsWithoutRef<'input'> & { width?: any }) {
   return <input className={styles.base} {...props} style={{ width: props.width }} />
