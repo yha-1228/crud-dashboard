2422931fb9bb10a4504af7678f6cf90ede108acd
長谷川 裕大 <gssbtsm999@gmail.com>
2021-01-01 18:09:13 +0900

    U

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,7 @@ const faker = require('faker')
 
 module.exports = () => {
   const db = { users: [] }
-  const size = 330
+  const size = 334
 
   for (let index = 1; index <= size; index++) {
     const item = {
