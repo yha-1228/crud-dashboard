213e4a6d0bd83b05c0414d85ec7091ceb9aae0c1
長谷川 裕大 <gssbtsm999@gmail.com>
2021-02-14 00:37:17 +0900

    U

--- src/components/UserList/Component.tsx
+++ src/components/UserList/Component.tsx
@@ -40,106 +40,101 @@ export function Component(props: Props) {
       </MainHeader>
 
       <Box>
-        {!isLoaded ? (
-          <Box pt="72px" textAlign="center">
-            <Spinner />
-          </Box>
-        ) : (
-          <>
-            <div
-              style={{
-                height: 'calc(100vh - 64px - 64px)',
-                border: '3px solid #eb425e',
-                overflowY: 'scroll',
-              }}
-            >
-              <TableContainer style={{ paddingLeft: 32, paddingRight: 32 }}>
-                <Table>
-                  <Thead>
-                    <tr>
-                      <Th align="left" scope="col">
-                        ID
-                      </Th>
-                      <Th align="left" scope="col" data-header="username">
-                        Username
-                      </Th>
-                      <Th align="left" scope="col" data-header="email">
-                        Email
-                      </Th>
-                      <Th align="left" scope="col"></Th>
-                      <Th align="left" scope="col"></Th>
-                    </tr>
-                  </Thead>
+        <Box display={isLoaded ? 'none' : 'block'} pt="72px" textAlign="center">
+          <Spinner />
+        </Box>
 
-                  <Tbody>
-                    {users.map((user) => (
-                      <tr key={user.id}>
-                        <Td>{user.id}</Td>
-                        <Td>{user.username}</Td>
-                        <Td>{user.email}</Td>
-                        <Td>
-                          <LinkButton size="small" to={`/users/${user.id}`}>
-                            <FontAwesomeIcon icon={faEdit} />
-                            &nbsp;&nbsp;Edit
-                          </LinkButton>
-                        </Td>
-                        <Td>
-                          <Button
-                            size="small"
-                            type="button"
-                            data-id={user.id}
-                            data-username={user.username}
-                            onClick={onDeleteClick}
-                            disabled={!isLoaded}
-                          >
-                            <FontAwesomeIcon icon={faTrash} />
-                            &nbsp;&nbsp;Delete
-                          </Button>
-                        </Td>
-                      </tr>
-                    ))}
-                  </Tbody>
-                </Table>
-              </TableContainer>
-            </div>
+        <Box
+          display={isLoaded ? 'block' : 'none'}
+          height="calc(100vh - 64px - 64px)"
+          border="2px solid black"
+          overflow="auto"
+        >
+          <TableContainer style={{ paddingLeft: 32, paddingRight: 32 }}>
+            <Table>
+              <Thead>
+                <tr>
+                  <Th align="left" scope="col">
+                    ID
+                  </Th>
+                  <Th align="left" scope="col" data-header="username">
+                    Username
+                  </Th>
+                  <Th align="left" scope="col" data-header="email">
+                    Email
+                  </Th>
+                  <Th align="left" scope="col"></Th>
+                  <Th align="left" scope="col"></Th>
+                </tr>
+              </Thead>
 
-            <Box
-              display="flex"
-              justifyContent="space-between"
-              alignItems="center"
-              height="64px"
-              px="32px"
-              border="2px solid #e04cfd"
-            >
-              <Paginate pageCount={pageCount} onPageChange={onPageChange} />
+              <Tbody>
+                {users.map((user) => (
+                  <tr key={user.id}>
+                    <Td>{user.id}</Td>
+                    <Td>{user.username}</Td>
+                    <Td>{user.email}</Td>
+                    <Td>
+                      <LinkButton size="small" to={`/users/${user.id}`}>
+                        <FontAwesomeIcon icon={faEdit} />
+                        &nbsp;&nbsp;Edit
+                      </LinkButton>
+                    </Td>
+                    <Td>
+                      <Button
+                        size="small"
+                        type="button"
+                        data-id={user.id}
+                        data-username={user.username}
+                        onClick={onDeleteClick}
+                        disabled={!isLoaded}
+                      >
+                        <FontAwesomeIcon icon={faTrash} />
+                        &nbsp;&nbsp;Delete
+                      </Button>
+                    </Td>
+                  </tr>
+                ))}
+              </Tbody>
+            </Table>
+          </TableContainer>
+        </Box>
 
-              <HStack spaceing={8}>
-                <Box display="inline-block" pr="8px" fontSize="12px" color="var(--color-gray-500)">
-                  Rows per page:
-                </Box>
-                <Box display="inline-block">
-                  <select
-                    className={css`
-                      & {
-                        font-size: 12px;
-                        color: var(--color-gray-500);
-                        cursor: pointer;
-                      }
-                    `}
-                    value={limit}
-                    onChange={onLimitChange}
-                  >
-                    {[10, 20, 30, 50, 100].map((value) => (
-                      <option key={value} value={value}>
-                        {value}
-                      </option>
-                    ))}
-                  </select>
-                </Box>
-              </HStack>
+        <Box
+          display={isLoaded ? 'flex' : 'none'}
+          justifyContent="space-between"
+          alignItems="center"
+          height="64px"
+          px="32px"
+          border="2px solid #e04cfd"
+        >
+          <Paginate pageCount={pageCount} onPageChange={onPageChange} />
+
+          <HStack spaceing={8}>
+            <Box display="inline-block" pr="8px" fontSize="12px" color="var(--color-gray-500)">
+              Rows per page:
             </Box>
-          </>
-        )}
+            <Box display="inline-block">
+              <select
+                className={css`
+                  & {
+                    font-size: 12px;
+                    color: var(--color-gray-500);
+                    cursor: pointer;
+                  }
+                `}
+                value={limit}
+                onChange={onLimitChange}
+              >
+                {[10, 20, 30, 50, 100].map((value) => (
+                  <option key={value} value={value}>
+                    {value}
+                  </option>
+                ))}
+              </select>
+            </Box>
+          </HStack>
+        </Box>
       </Box>
     </>
   )
