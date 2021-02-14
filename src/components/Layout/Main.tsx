0a1d59587e0fc498fd907aa3f6343debe173bfa8
長谷川 裕大 <gssbtsm999@gmail.com>
2021-02-14 18:00:39 +0900

    U

--- src/components/Layout/Main.tsx
+++ src/components/Layout/Main.tsx
@@ -3,19 +3,8 @@ import React from 'react'
 
 export function Main({ children }: { children: React.ReactNode }) {
   return (
-    <div
-      className={css({
-        flexGrow: 1,
-        flexShrink: 1,
-      })}
-    >
-      <div
-        className={css({
-          width: 'calc(100vw - 240px)',
-          height: '100vh',
-          overflowY: 'auto',
-        })}
-      >
+    <div className={css({ flexGrow: 1, flexShrink: 1 })}>
+      <div className={css({ width: 'calc(100vw - 240px)', height: '100vh', overflowY: 'auto' })}>
         {children}
       </div>
     </div>
