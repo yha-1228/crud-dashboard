67b764ab356bd07fd37f9155837a5969aac71576
長谷川 裕大 <gssbtsm999@gmail.com>
2021-02-14 20:51:48 +0900

    U

--- src/components/shared/Stack.tsx
+++ src/components/shared/Stack.tsx
@@ -1,39 +1,39 @@
 import { css } from '@emotion/css'
 
-type Props = {
+type StackProps = {
   direction?: 'vertical' | 'horizontal'
   spaceing: number
   children: React.ReactNode[]
 }
 
-export function VStack({ spaceing, children }: Props) {
+export function VStack({ spaceing, children }: StackProps) {
   return (
     <div
-      className={css`
-        & > * {
-          margin-bottom: ${spaceing}px;
-        }
-        & > *:last-child {
-          margin-bottom: 0;
-        }
-      `}
+      className={css({
+        '& > *': {
+          marginBottom: `${spaceing}px`,
+        },
+        '& > *:last-child': {
+          marginBottom: 0,
+        },
+      })}
     >
       {children}
     </div>
   )
 }
 
-export function HStack({ spaceing, children }: Props) {
+export function HStack({ spaceing, children }: StackProps) {
   return (
     <div
-      className={css`
-        & > * {
-          margin-right: ${spaceing}px;
-        }
-        & > *:last-child {
-          margin-right: 0;
-        }
-      `}
+      className={css({
+        '& > *': {
+          marginRight: `${spaceing}px`,
+        },
+        '& > *:last-child': {
+          marginRight: 0,
+        },
+      })}
     >
       {children}
     </div>
