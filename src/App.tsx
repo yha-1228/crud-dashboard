2e396b182cb06cc94e7e73df69fd3cbc1fbd7a89
長谷川 裕大 <gssbtsm999@gmail.com>
2021-03-14 20:03:41 +0900

    U

--- src/App.tsx
+++ src/App.tsx
@@ -9,22 +9,18 @@ import './styles/base.css'
 import './styles/utils.css'
 
 // Pages
-import { UserEditPage } from './pages/UserEditPage'
 import { HomePage } from './pages/HomePage'
 import { UserListPage } from './pages/UserListPage'
-import { UserCreatePage } from './pages/UserCreatePage'
 import { Page01Page } from './pages/Page01Page'
 import { Page02Page } from './pages/Page02Page'
 import { Page03Page } from './pages/Page03Page'
 
 const routes = [
-  { path: '/', exact: true, component: HomePage },
-  { path: '/users', exact: true, component: UserListPage },
-  { path: '/users/create', exact: true, component: UserCreatePage },
-  { path: '/users/:id', exact: true, component: UserEditPage },
-  { path: '/page01', exact: true, component: Page01Page },
-  { path: '/page02', exact: true, component: Page02Page },
-  { path: '/page03', exact: true, component: Page03Page },
+  { path: '/', exact: true, children: <HomePage /> },
+  { path: '/users', exact: true, children: <UserListPage /> },
+  { path: '/page01', exact: true, children: <Page01Page /> },
+  { path: '/page02', exact: true, children: <Page02Page /> },
+  { path: '/page03', exact: true, children: <Page03Page /> },
 ]
 
 export default function App() {
