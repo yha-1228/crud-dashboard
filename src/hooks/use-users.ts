20799e1cedc853270624fd67ce00814e6ded929e
yuta <gssbtsm999@gmail.com>
2023-01-01 01:24:28 +0900

    refactor: add error state

--- src/hooks/use-users.ts
+++ src/hooks/use-users.ts
@@ -1,24 +1,69 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useReducer } from 'react'
 import UserAPI, { UserGetParams } from '../api/UserAPI'
 import { User } from '../types'
 
+type State = {
+  data: User[]
+  totalCount: number
+  isLoaded: boolean
+  error: Error | null
+}
+
+type Action =
+  | { type: 'init' }
+  | { type: 'done'; payload: { data: User[]; totalCount: number } }
+  | { type: 'error'; error: Error }
+
+const reducer = (state: State, action: Action): State => {
+  if (action.type === 'init') {
+    return state
+  }
+
+  if (action.type === 'done') {
+    return {
+      data: action.payload.data,
+      totalCount: action.payload.totalCount,
+      isLoaded: true,
+      error: null,
+    }
+  }
+
+  if (action.type === 'error') {
+    return {
+      ...state,
+      isLoaded: true,
+      error: action.error,
+    }
+  }
+
+  return state
+}
+
+const initialState: State = {
+  data: [],
+  totalCount: 0,
+  isLoaded: false,
+  error: null,
+}
+
 export function useUsers(params: UserGetParams) {
-  const [data, setData] = useState<User[]>([])
-  const [totalCount, setTotalCount] = useState(0)
-  const [isLoaded, setIsLoaded] = useState(false)
-  // TODO: add error state
+  const [state, dispatch] = useReducer(reducer, initialState)
 
   const [fetchCount, setFetchCount] = useState(0)
 
   useEffect(() => {
     UserAPI.findMany({ _start: params._start, _limit: params._limit })
       .then((response) => {
-        setIsLoaded(true)
-        setData(response.data)
-        setTotalCount(response.totalCount)
+        dispatch({
+          type: 'done',
+          payload: { data: response.data, totalCount: response.totalCount },
+        })
       })
       .catch((error) => {
-        console.log('error :>> ', error.message)
+        dispatch({
+          type: 'error',
+          error,
+        })
       })
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [fetchCount, params._start, params._limit])
@@ -28,9 +73,7 @@ export function useUsers(params: UserGetParams) {
   }
 
   return {
-    data,
-    totalCount,
-    isLoaded,
+    ...state,
     refetch,
   }
 }
