a6615a2ef9da00de4eaa7a77a0a9f42bc56af3c0
yuta <gssbtsm999@gmail.com>
2023-01-09 08:39:42 +0900

    chore: add emotion/babel-plugin

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,14 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react({ jsxImportSource: '@emotion/react' })],
+  plugins: [
+    react({
+      jsxImportSource: '@emotion/react',
+      babel: {
+        plugins: ['@emotion/babel-plugin'],
+      },
+    }),
+  ],
   server: {
     port: 3000,
   },
