e9e7ecfbd103153e4aaebc5daaa4a5744f73534a
yuta <gssbtsm999@gmail.com>
2023-01-08 21:52:17 +0900

    chore: remove cn.ts

--- src/utils/cn.ts
+++
@@ -1,8 +0,0 @@
-type ClassInput = string | undefined | null | boolean;
-
-/**
- * concat classnames
- */
-export const cn = (...classInputs: ClassInput[]) => {
-  return classInputs.filter(Boolean).join(' ');
-};
