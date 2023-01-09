5742165a9100fcf8909c301dac7f8b038e26e87b
yuta <gssbtsm999@gmail.com>
2023-01-09 14:48:33 +0900

    chore: install vite-plugin-checker

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import checker from 'vite-plugin-checker';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    checker({ typescript: true }),
     react({
       jsxImportSource: '@emotion/react',
       babel: {
