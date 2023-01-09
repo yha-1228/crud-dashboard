08739ea6ad7d5740cd46296a8562831350835ebc
yuta <gssbtsm999@gmail.com>
2023-01-09 14:35:00 +0900

    chore: 型定義を厳密に

--- src/styles/utils.ts
+++ src/styles/utils.ts
@@ -2,9 +2,18 @@ import { theme } from './theme';
 
 type ValueOf<T> = T[keyof T];
 
+/**
+ * Union to Intersection
+ */
+type Join<U> = (U extends unknown ? (k: U) => void : never) extends (
+  k: infer I
+) => void
+  ? I
+  : never;
+
 export const getTheme = (
   first: keyof typeof theme,
-  second: keyof ValueOf<typeof theme>
+  second: keyof Join<ValueOf<typeof theme>>
 ) => {
   return `var(--${first}-${second})`;
 };
